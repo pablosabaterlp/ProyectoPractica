0e963f09520ed0332b7c1439805ba16304cff743
pablosabaterlp <157770008+pablosabaterlp@users.noreply.github.com>
2024-05-20 09:29:10 -0400

    Añadido en Puesto Ocupado Codigo

--- FaceRecognitionAzure/Codigo Test/puestoOcupado.py
+++ FaceRecognitionAzure/Codigo Test/puestoOcupado.py
@@ -2,9 +2,11 @@ from azure.cognitiveservices.vision.customvision.prediction import CustomVisionP
 from msrest.authentication import ApiKeyCredentials
 import time, requests, imageio, cv2, os
 
-prediction_key = "predicition key"
-endpoint = "endpoint"
-project_id = "custom vis project id"
+training_key = os.environ.get('VISION_TRAINING_KEY')
+training_endpoint = os.environ.get('VISION_TRAINING_ENDPOINT')
+prediction_key = os.environ.get('VISION_PREDICTION_KEY')
+prediction_endpoint = os.environ.get('VISION_PREDICTION_ENDPOINT')
+resource_id = os.environ.get('VISION_PREDICTION_RESOURCE_ID')
 
 credentials = ApiKeyCredentials(in_headers={"Prediction-key":prediction_key})
 predictor = CustomVisionPredictionClient(endpoint,credentials)
