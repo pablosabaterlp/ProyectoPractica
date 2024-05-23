c2bdc5eba44cf98ee95bc89d6f4cadc6bfb08bf5
pablosabaterlp <157770008+pablosabaterlp@users.noreply.github.com>
2024-05-23 08:58:57 -0400

    Update puestoOcupado.py

--- FaceRecognitionAzure/Codigo Test/puestoOcupado.py
+++ FaceRecognitionAzure/Codigo Test/puestoOcupado.py
@@ -2,12 +2,12 @@ from azure.cognitiveservices.vision.customvision.prediction import CustomVisionP
 from msrest.authentication import ApiKeyCredentials
 import os, time, uuid, requests, cv2
 
-training_key ='key'
-training_endpoint ='endpoint'
-prediction_key = 'key'
-prediction_endpoint = 'endpoint'
-project_id = 'id'
-publish_iteration_name = 'Iteration3'
+training_key = 'dc04ada586134feaa1fdaa543c592fdd'
+training_endpoint ='https://chls1zu1cvbpocaaicrit001.cognitiveservices.azure.com/'
+prediction_key = 'f97fa93036b5479da5f70f31150d6f0d'
+prediction_endpoint = 'https://chls1zu1cvbpocaaicrit001-prediction.cognitiveservices.azure.com/'
+project_id = '7459c025-eac8-4078-ab8a-5407dee1987f'
+publish_iteration_name = 'Iteration4'
 
 # Now there is a trained endpoint that can be used to make a prediction
 prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": prediction_key})
@@ -41,7 +41,7 @@ def analyze_image(file_path):
                 unoccupied_list.append(prediction)
                 unoccupied_prob_total += prediction.probability
         elif prediction.tag_name == "Chair":
-            if prediction.probability > 0.9:
+            if prediction.probability > 0.95:
                 total_spots += 1
         #if prediction.probability > 0.8:
             #print(f"\t{prediction.tag_name}: {prediction.probability * 100:.2f}% "
@@ -52,22 +52,26 @@ def analyze_image(file_path):
 
     occupied_count = len(occupied_list)
     unoccupied_count = len(unoccupied_list)
-    print(f"# of Spaces: {total_spots}")
-    print(f"# of Occupied Spots: {occupied_count}")
+    if total_spots > occupied_count and total_spots > unoccupied_count:
+        print(f"# de Puestos: {total_spots}")
+    elif total_spots < occupied_count or total_spots < unoccupied_count:
+        print(f"# de Puestos: {occupied_count + unoccupied_count}")
+    
+    print(f"# de Puestos Occupados: {occupied_count}")
     if occupied_count > 0:
         occupied_avg_prob = occupied_prob_total / occupied_count * 100
-        print(f"Average Occupied Probability: {occupied_avg_prob:.2f}")
+        print(f"Probabilidad de Acertaje: {occupied_avg_prob:.2f}")
     else:
         occupied_avg_prob = 0
-        print("No occupied spots detected.")
+        print("No se detectan sitios ocupados.")
 
-    print(f"# of Unoccupied Spots: {unoccupied_count}")
+    print(f"# de Puestos Desocupados: {unoccupied_count}")
     if unoccupied_count > 0:
         unoccupied_avg_prob = unoccupied_prob_total / unoccupied_count * 100
-        print(f"Average Unoccupied Probability: {unoccupied_avg_prob:.2f}")
+        print(f"Probabilidad de Acertaje: {unoccupied_avg_prob:.2f}")
     else:
         unoccupied_avg_prob = 0
-        print("No unoccupied spots detected.")
+        print("No se detectan sitios desocupados.")
 
     return {
         "occupied_count": occupied_count,
