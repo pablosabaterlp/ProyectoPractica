6ffa0f5c9f48a603f267e7221bf63f5d406a2256
pablosabaterlp <157770008+pablosabaterlp@users.noreply.github.com>
2024-06-07 10:09:57 -0400

    Delete FaceRecognitionAzure/Codigo Test/read_line.py

--- FaceRecognitionAzure/Codigo Test/read_line.py
+++
@@ -1,7 +0,0 @@
-def read_line(path, line):
-    with open(path, 'r') as file:
-        lines = file.readlines()
-        if 0 <= line < len(lines):
-            return lines[line].strip()
-        else:
-            raise ValueError("Line Number out of Range")
